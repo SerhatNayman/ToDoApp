b14e83a930b387f097487c3afbd8d64eea8026e1
Serhat Nayman <serhatnayman@gmail.com>
2022-09-20 11:43:20 +0300

    add SplashScreen

--- app/src/main/java/com/serosoft/todoapp/MainActivity.kt
+++ app/src/main/java/com/serosoft/todoapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.serosoft.todoapp
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1000)
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
